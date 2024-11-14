4cef552906b145566e352e48178efe7017c2b0f0
Shanmugapriya <shanmugapriya8004@gmail.com>
2024-11-14 11:04:11 +0530

    loan.py

--- loan-approval prediction/loan.py
+++ loan-approval prediction/loan.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-file_path = r'C:\Users\Downloads\loan-prediction.csv'
+file_path = r"C:\Users\hp\Downloads\loan-prediction.csv"
 data = pd.read_csv(file_path)
 print("First 5 rows of the dataset:")
 print(data.head())
